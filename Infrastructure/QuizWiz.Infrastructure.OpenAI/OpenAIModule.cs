575e80d207184513860cddd446c7cba6d86cc4e8
Michael Z <michaelzhou09@hotmail.com>
2023-11-22 08:51:09 +1300

    Add Infrastructure OpenAI project

---
+++ Infrastructure/QuizWiz.Infrastructure.OpenAI/OpenAIModule.cs
@@ -0,0 +1,16 @@
+﻿//using Microsoft.Extensions.DependencyInjection;
+
+//namespace QuizWiz.Infrastructure.OpenAI;
+
+//public static class OpenAIModule
+//{
+//    public static IServiceCollection AddOpenAIServices(this IServiceCollection services, string proxyUrl, string apiKey, string githubAlias)
+//    {
+//        services.AddSingleton<IOpenAIService, OpenAIService>(provider =>
+//        {
+//            return new OpenAIService(proxyUrl, apiKey, githubAlias);
+//        });
+
+//        return services;
+//    }
+//}
