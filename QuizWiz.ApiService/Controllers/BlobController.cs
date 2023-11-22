fee10b41afbe94ea641359c8a6c8e408fb7b1da2
gauravpathak91 <gauravpathak91@gmail.com>
2023-11-22 18:21:28 +1300

    Add Blob Service

---
+++ QuizWiz.ApiService/Controllers/BlobController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizWiz.Persistence.BlobStorage;
+
+namespace QuizWiz.ApiService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlobController : ControllerBase
+    {
+        private readonly IBlobService _blobService;
+
+        public BlobController(IBlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
+        [HttpGet("download/{articleName}")]
+        public async Task<IActionResult> GetArticle(string articleName)
+        {
+            try
+            {
+                var stream = await _blobService.DownloadBlobAsync(articleName);
+
+                // Return the stream as part of the response
+                return File(stream, "application/octet-stream", articleName);
+            }
+            catch (Exception ex)
+            {
+                // Handle errors and return appropriate response
+                Console.WriteLine($"Error downloading blob: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpPost("upload/article")]
+        public async Task<IActionResult> UploadArticleAsync(IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                {
+                    return BadRequest("File is empty or null");
+                }
+
+                // Generate a unique blob name or use the original file name
+                var blobName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                var fileBytes = StreamToByteArray(file.OpenReadStream());
+
+                // Upload the blob and get the URI
+                var blobUri = _blobService.CreateBlobAsync(fileBytes, file.FileName);
+
+                // You can return the URI in the response
+                return Ok(new { BlobUri = blobUri });
+            }
+            catch (Exception ex)
+            {
+                // Handle errors and return appropriate response
+                Console.WriteLine($"Error uploading blob: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        private static byte[] StreamToByteArray(Stream stream)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ QuizWiz.ApiService/DependencyInjectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Blobs;
+using QuizWiz.ApiService.Settings;
+using QuizWiz.Persistence.BlobStorage;
+
+namespace QuizWiz.ApiService
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection ConfigureDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+            // Add dependencies here
+            var settings = configuration.GetSection(ApiServiceSettings.SectionName).Get<ApiServiceSettings>();
+
+            services.AddSingleton(x => new BlobServiceClient(settings.BlobStorageSettings.ConnectionString));
+
+            services.AddSingleton(settings.BlobStorageSettings);
+
+            // Register BlobServices
+
+            services.AddScoped<IBlobService, BlobService>();
+
+            return services;
+        }
+    }
+}
