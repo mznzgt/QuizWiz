a985e0cf2f54f093f750b80726baa34e7ad2357a
Gaurav <gauravpathak91@gmail.com>
2023-11-23 20:36:20 +1300

    Add blob service (#9)
    Co-authored-by: senthil <sksanthoshg94@gmail.com>

---
+++ Application/QuizWiz.Application.QuizGenerator/Queries/GetArticleQuery.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using QuizWiz.Persistence.BlobStorage;
+
+namespace QuizWiz.Application.QuizGenerator.Queries
+{
+    public class GetArticleQuery : IRequest<Stream>
+    {
+        public string BlobName { get; set; }
+
+        public GetArticleQuery(string blobName)
+        {
+            BlobName = blobName;
+        }
+    }
+
+    public class GetArticleQueryHandler : IRequestHandler<GetArticleQuery, Stream>
+    {
+        private readonly IBlobService _blobService;
+
+        public GetArticleQueryHandler(IBlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
+        public async Task<Stream> Handle(GetArticleQuery request, CancellationToken cancellationToken)
+        {
+            var stream = await _blobService.DownloadBlobAsync(request.BlobName);
+
+            return stream;
+        }
+    }
+}
---
+++ Presentation/QuizWiz.ApiService/Controllers/BlobController.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using QuizWiz.Application.QuizGenerator.Commands;
+using QuizWiz.Application.QuizGenerator.Queries;
+
+namespace QuizWiz.ApiService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlobController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BlobController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("download/{articleName}")]
+        public async Task<IActionResult> GetArticle(string articleName)
+        {
+            try
+            {
+                var query = new GetArticleQuery(articleName);
+                var result = await _mediator.Send(query);
+                // Return the stream as part of the response
+                return File(result, "application/octet-stream", articleName);
+            }
+            catch (Exception ex)
+            {
+                // Handle errors and return appropriate response
+                Console.WriteLine($"Error downloading blob: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpPost("upload/article")]
+        public async Task<IActionResult> UploadArticleAsync(IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                {
+                    return BadRequest("File is empty or null");
+                }
+
+                var request = new AddArticleCommand(file.FileName, file.OpenReadStream());
+
+                var result = await _mediator.Send(request);
+
+                // You can return the URI in the response
+                return Ok(new { BlobUri = result });
+            }
+            catch (Exception ex)
+            {
+                // Handle errors and return appropriate response
+                Console.WriteLine($"Error uploading blob: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+
+    }
+}
