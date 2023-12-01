dc7f5cfbb60f0191b1e1f6b63cd250302a05426a
senthil <sksanthoshg94@gmail.com>
2023-12-02 12:21:54 +1300

    Register flow (#12)
    * Register flow
    
    * Update Presentation/QuizWiz.ApiService/appsettings.json
    
    ---------
    
    Co-authored-by: Gaurav <gauravpathak91@gmail.com>

---
+++ Presentation/QuizWiz.Web/Model/Response.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizWiz.Web.Model
+{
+    public class Response
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public Dictionary<string, List<string>> Errors { get; set; }
+    }
+    
+}
---
+++ Presentation/QuizWiz.Web/Services/AuthService.cs
@@ -0,0 +1,50 @@
+﻿using QuizWiz.Web.Components.Pages;
+using QuizWiz.Web.Model;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.IO;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace QuizWiz.Web.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public Task<LoginResult> Login(Model.Login loginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<RegisterResult> Register(Register register)
+        {
+            var httpClient = _httpClientFactory.CreateClient("Auth");
+
+            using StringContent registerContent = new(System.Text.Json.JsonSerializer.Serialize(register),Encoding.UTF8,"application/json");
+            using HttpResponseMessage response = await httpClient.PostAsync("/account/register", registerContent);
+            
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var res = JsonConvert.DeserializeObject<Response>(jsonResponse);
+
+            return new RegisterResult
+            {
+                Successful = response.IsSuccessStatusCode,
+                Response = res
+            };
+        }
+    }
+}
